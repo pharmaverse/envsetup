492a444f6560bd33db0a86d1a3d1cca1fefcffe6
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-02-12 01:27:12 +0000

    Update tests for refactoring of set_autos

--- tests/testthat/test-autos.R
+++ tests/testthat/test-autos.R
@@ -1,10 +1,7 @@
 
 tmpdir <- base::tempdir()
 
-Sys.setenv(ENVSETUP_ENVIRON = "DEV")
-
 Sys.setenv(testpath = (tmpdir))
-
 
 system(paste0("cp ", testthat::test_path("man/testdir/*"), " ", tmpdir, " -r"))
 
@@ -12,85 +9,72 @@ envsetup_config <- config::get(
   file = testthat::test_path("man/_envsetup_testthat.yml")
 )
 
+# Dev tests
+Sys.setenv(ENVSETUP_ENVIRON = "DEV")
 
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.1", {
-  set_autos(envsetup_config$autos)
+#' @editor Mike Stackhouse
+#' @editDate 2023-02-11
+test_that("Autos set and test_dev from highest level appears correctly", {
+  do.call(set_autos, envsetup_config$autos)
   expect_equal(c(test_dev()), c("Test of dev autos"))
+  expect_equal(c(test_global()), c("Test of global autos"))
 })
+
 #' @editor Aidan Ceney
 #' @editDate 2022-05-12
 test_that("1.2", {
-  expect_error(set_autos(1), "Paths must be directories")
+  expect_error(set_autos(1), "Paths provided for autos must be directories")
 })
 
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.3", {
-  envsetup_config_tmp <- envsetup_config
-  envsetup_config_tmp$autos[2] <- envsetup_config$autos[1]
-  set_autos(envsetup_config_tmp$autos)
-  expect_equal(
-    c(test_dev(), test_prod()),
-    c("Test of dev autos", "Test of prod autos")
-  )
-  expect_error(test_qa())
-})
+# Detatch and re-setup for QA now
+detach_autos()
+Sys.setenv(ENVSETUP_ENVIRON = "QA")
 
-
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.4", {
-  print(envsetup_config$autos)
-  set_autos(envsetup_config$autos)
-  expect_equal(mtcars, iris)
-})
-
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.5", {
-  set_autos(envsetup_config$autos)
-  expect_equal(test_prod(), "Test of prod autos")
-  expect_equal(test_prod2(), "Test of prod autos second")
-})
-
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.6", {
-  set_autos(envsetup_config$autos)
-  expect_equal(mtcars, iris)
-})
-
-
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.7", {
-  Sys.setenv(ENVSETUP_ENVIRON = "QA")
-  set_autos(envsetup_config$autos)
-  expect_error(test_dev())
-  Sys.setenv(ENVSETUP_ENVIRON = "PROD")
-  set_autos(envsetup_config$autos)
-  expect_error(test_dev())
-})
-
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("1.8", {
-  Sys.setenv(ENVSETUP_ENVIRON = "QA")
-  set_autos(envsetup_config$autos)
-  expect_error(test_dev())
+#' @editor Mike Stackhouse
+#' @editDate 2023-02-11
+test_that("Setting environment to QA filters out dev autos", {
+  do.call(set_autos, envsetup_config$autos)
   expect_equal(
     c(test_qa(), test_prod()),
     c("Test of qa autos", "Test of prod autos")
   )
+  expect_error(test_dev())
+  expect_equal(c(test_global()), c("Test of global autos"))
 })
 
 
-#' @editor Aidan Ceney
-#' @editDate 2022-05-12
-test_that("2.1", {
-  detach_autos(c("autos:QA", "autos:PROD"))
+#' @editor Mike Stackhouse
+#' @editDate 2023-02-11
+test_that("Data output in namespace appears", {
+  do.call(set_autos,envsetup_config$autos)
+  expect_equal(mtcars, iris)
+})
+
+#' @editor Mike Stackhouse
+#' @editDate 2023-02-11
+test_that("Functions in higher level hierarchy export and multiple functions may be captured", {
+  do.call(set_autos,envsetup_config$autos)
+  expect_equal(test_prod(), "Test of prod autos")
+  expect_equal(test_prod2(), "Test of prod autos second")
+})
+
+#' @editor Mike Stackhouse
+#' @editDate 2022-02-11
+test_that("set_autos effectively clears and resets namespace", {
+  Sys.setenv(ENVSETUP_ENVIRON = "QA")
+  do.call(set_autos,envsetup_config$autos)
+  expect_error(test_dev())
+  expect_equal(c(test_global()), c("Test of global autos"))
+  Sys.setenv(ENVSETUP_ENVIRON = "PROD")
+  do.call(set_autos,envsetup_config$autos)
+  expect_error(test_qa())
+  expect_equal(c(test_global()), c("Test of global autos"))
+})
+
+#' @editor Mike Stackhouse
+#' @editDate 2023-02-11
+test_that("Autos no longer exist when detached", {
+  detach_autos()
   expect_error(test_qa())
   expect_error(test_prod())
 })
