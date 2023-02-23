e84f478cc33fc4e906754709220f9fbcc3fa0639
Michael Stackhouse <michaelstackhouse0614@gmail.com>
2023-02-23 15:01:14 -0500

    Update R/autos.R
    Co-authored-by: Nicholas Masel <61123199+nicholas-masel@users.noreply.github.com>

--- R/autos.R
+++ R/autos.R
@@ -112,7 +112,7 @@ attach_auto <- function(path, name) {
 
   if (!(dir.exists(path) || file.exists(path))) {
     # Check if the auto actually exists
-    warning(sprintf("Autos path specified in _envsetup.yml does not exist: %s = %s", name, path),
+    warning(sprintf("An autos path specified in your envsetup configuration file does not exist: %s = %s", name, path),
          call.=FALSE)
   } else if (file.exists(path) && !dir.exists(path)) {
     # if file, source it
